fb84e119aef06955c87cde9e7e54514b9f516244
Hoang Truong <hoangtruong@Hoangs-MacBook-Pro.local>
2020-08-14 15:56:01 +0200

    add startup message to order srv

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -11,6 +11,7 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
+  console.log("Starting...");
   //CHECK IF ENVIRONMENT VARIABLE IS AVAILABLE
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_Key must be definded");
